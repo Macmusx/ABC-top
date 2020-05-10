7ab1d91d6d4a6ba7243034437df3271759e99092
jalbaandrei <snury1997>
2020-05-10 18:44:32 +0300

    some changes

--- main.cpp
+++ main.cpp
@@ -9,55 +9,54 @@
 #define alpha 418.982887
 
 /* Parametrii de control*/
-#define NP 40 /* Dimensiunea coloniei*/
-#define FoodNumber NP / 2 /* Numarul resurselor de mancare = jumatate din dimensiunea coloniei*/
+#define columnDimension 40 /* Dimensiunea coloniei*/
+#define numberOfFoodResources columnDimension / 2 /* Numarul resurselor de mancare = jumatate din dimensiunea coloniei*/
 #define limit 100 /* Limita pentru counter-ul de imbunatatire*/
-#define maxCycle 3000 /* Nr de cicluri pentru cautarea hranei*/
+#define maxNumberOfCycles 3000 /* Numarul de cicluri pentru cautarea hranei*/
 
 /* Constante specifice problemei*/
-#define D 50 /* Numar de parametrii care trebuie optimizati*/
-#define lb -5.12 /* Limita inferioara */
-#define ub 5.12 /* Limita superioara*/
+#define noOfOptmimizedParams 50 /* Numar de parametrii care trebuie optimizati*/
+#define lowLimit -5.12 /* Limita inferioara */
+#define upperLimit 5.12 /* Limita superioara*/
 
-#define runtime 30 /* De cate ori e rulat algoritmul*/
+#define numberOfRuns 10 /* De cate ori este rulat algoritmul*/
 
-double Foods[FoodNumber][D]; /* Populatia mancarii*/
-double f[FoodNumber]; /* Tine valoriile functiei mancarii*/
-double fitness[FoodNumber]; /* Tine valorile fitness-ului pentru fiecare mancare*/
-double trial[FoodNumber]; /* Retine valorile unde solutiile nu pot fi imbunatatite (counter-ul de imbunatatire)*/
-double prob[FoodNumber]; /* Contine probabilitatile ca o sursa de mancare sa fie aleasa*/
-double solution[D]; /* Noua solutie produsa v_{ij}=x_{ij}+\phi_{ij}*(x_{ij}-x_{kj}), j - parametru random, k - solutie random, diferita de i*/
-double ObjValSol; /* Valoarea functiei a noii solutii*/
-double FitnessSol; /* Valoarea fitness-ului noii solutii*/
-int neighbour; /* Corespunde k-ului din ecuatia v_{ij}=x_{ij}+\phi_{ij}*(x_{kj}-x_{ij})*/
-int param2change; /* Corespunde lui j*/
-double GlobalMin; /* Optimum*/
-double GlobalParams[D]; /* Parametrii solutiei optium*/
-double GlobalMins[runtime]; /* Tine GlobalMin pentru mai multe run-uri*/
+double Foods[numberOfFoodResources][noOfOptmimizedParams]; /* Populatia mancarii*/
+double f[numberOfFoodResources]; /* Stocheaza valorile functiei mancarii*/
+double fitness[numberOfFoodResources]; /* Tine valorile fitness-ului pentru fiecare mancare*/
+double improvementFactor[numberOfFoodResources]; /* Retine valorile unde solutiile nu pot fi imbunatatite (counter-ul de imbunatatire)*/
+double probForFoodResToBeChoosen[numberOfFoodResources]; /* Contine probabilitatile ca o sursa de mancare sa fie aleasa*/
+double solution[noOfOptmimizedParams]; /* Noua solutie produsa v_{ij}=x_{ij}+\phi_{ij}*(x_{ij}-x_{kj}), j - parametru random, k - solutie random, diferita de i*/
+double functionNewVal; /* Valoarea functiei noii solutii*/
+double fitnessVal; /* Valoarea fitness-ului noii solutii*/
+int proximity; /* Corespunde k din ecuatia v_{ij}=x_{ij}+\phi_{ij}*(x_{kj}-x_{ij})*/
+int paramForChange; /* Parametrul care trebuie schimbat, corespunde lui j*/
+double GlobalMin; /* Minimul global - valoarea optima*/
+double GlobalParams[noOfOptmimizedParams]; /* Parametrii solutiei minimului global ( a valorii optime )*/
+double GlobalMins[numberOfRuns]; /* Stocheaza valorile pentru GlobalMin pentru fiecare rulare*/
 double r; /* numar random intre [0, 1)*/
 
 /* Settup pentru functia folosita*/
-typedef double (*FunctionCallback)(double sol[D]);
+typedef double (*FunctionCallback)(double sol[noOfOptmimizedParams]);
 
 /* Functii pentru testat*/
-double Rosenbrock(double sol[D]);
-double Griewank(double sol[D]);
-double Rastrigin(double sol[D]);
-double Ackley(double sol[D]);
-double Schwefel(double sol[D]);
+double Rosenbrock(double sol[noOfOptmimizedParams]);
+double Griewank(double sol[noOfOptmimizedParams]);
+double Rastrigin(double sol[noOfOptmimizedParams]);
+double Ackley(double sol[noOfOptmimizedParams]);
 
 /* Functia folosita*/
-FunctionCallback function = &Schwefel;
+FunctionCallback function = &Ackley;
 
 /* Functia pentru fitness*/
-double CalculateFitness(double fun)
+double CalculateFitness(double index)
 {
     double result = 0;
-    if (fun >= 0) {
-        result = 1 / (fun + 1);
+    if (index >= 0) {
+        result = 1 / (index + 1);
     }
     else {
-        result = 1 + fabs(fun);
+        result = 1 + fabs(index);
     }
     return result;
 }
@@ -67,10 +66,10 @@ void MemorizeBestSource()
 {
     int i, j;
 
-    for (i = 0; i < FoodNumber; i++) {
+    for (i = 0; i < numberOfFoodResources; i++) {
         if (f[i] < GlobalMin) {
             GlobalMin = f[i];
-            for (j = 0; j < D; j++)
+            for (j = 0; j < noOfOptmimizedParams; j++)
                 GlobalParams[j] = Foods[i][j];
         }
     }
@@ -80,25 +79,25 @@ void MemorizeBestSource()
 void init(int index)
 {
     int j;
-    for (j = 0; j < D; j++) {
+    for (j = 0; j < noOfOptmimizedParams; j++) {
         r = ((double)rand() / ((double)(RAND_MAX) + (double)(1)));
-        Foods[index][j] = r * (ub - lb) + lb;
+        Foods[index][j] = r * (upperLimit - lowLimit) + lowLimit;
         solution[j] = Foods[index][j];
     }
     f[index] = function(solution);
     fitness[index] = CalculateFitness(f[index]);
-    trial[index] = 0;
+    improvementFactor[index] = 0;
 }
 
 /* Initializeaza sursele de mancare*/
 void initial()
 {
     int i;
-    for (i = 0; i < FoodNumber; i++) {
+    for (i = 0; i < numberOfFoodResources; i++) {
         init(i);
     }
     GlobalMin = f[0];
-    for (i = 0; i < D; i++)
+    for (i = 0; i < noOfOptmimizedParams; i++)
         GlobalParams[i] = Foods[0][i];
 }
 
@@ -106,47 +105,47 @@ void SendEmployedBees()
 {
     int i, j;
     /* Etapa albinelor lucratoare*/
-    for (i = 0; i < FoodNumber; i++) {
+    for (i = 0; i < numberOfFoodResources; i++) {
         /* Parametrul care trebuie schimbat este ales aleator*/
         r = ((double)rand() / ((double)(RAND_MAX) + (double)(1)));
-        param2change = (int)(r * D);
+        paramForChange = (int)(r * noOfOptmimizedParams);
 
         /* O solutie aleasa random creaza o solutie derivata a solutiei i*/
         r = ((double)rand() / ((double)(RAND_MAX) + (double)(1)));
-        neighbour = (int)(r * FoodNumber);
+        proximity = (int)(r * numberOfFoodResources);
 
         /* Solutia aleasa random trebuie sa fie diferita de i*/
-        while (neighbour == i) {
+        while (proximity == i) {
             r = ((double)rand() / ((double)(RAND_MAX) + (double)(1)));
-            neighbour = (int)(r * FoodNumber);
+            proximity = (int)(r * numberOfFoodResources);
         }
-        for (j = 0; j < D; j++) {
+        for (j = 0; j < noOfOptmimizedParams; j++) {
             solution[j] = Foods[i][j];
         }
 
         /* v_{ij}=x_{ij}+\phi_{ij}*(x_{ij}-x_{kj})*/
         r = ((double)rand() / ((double)(RAND_MAX) + (double)(1)));
-        solution[param2change] = Foods[i][param2change] + (Foods[i][param2change] - Foods[neighbour][param2change]) * (r - 0.5) * 2;
+        solution[paramForChange] = Foods[i][paramForChange] + (Foods[i][paramForChange] - Foods[proximity][paramForChange]) * (r - 0.5) * 2;
 
         /* Daca valoarea este inafara limitelor se seteaza ca valoare limita*/
-        if (solution[param2change] < lb)
-            solution[param2change] = lb;
-        if (solution[param2change] > ub)
-            solution[param2change] = ub;
-        ObjValSol = function(solution);
-        FitnessSol = CalculateFitness(ObjValSol);
+        if (solution[paramForChange] < lowLimit)
+            solution[paramForChange] = lowLimit;
+        if (solution[paramForChange] > upperLimit)
+            solution[paramForChange] = upperLimit;
+        functionNewVal = function(solution);
+        fitnessVal = CalculateFitness(functionNewVal);
 
-        if (FitnessSol > fitness[i]) {
+        if (fitnessVal > fitness[i]) {
             /* Daca solutia derivata este mai buna decat cea curenta, noua solutie devine cea derivata si reseteaza counter-ul de imbunatatire*/
-            trial[i] = 0;
-            for (j = 0; j < D; j++)
+            improvementFactor[i] = 0;
+            for (j = 0; j < noOfOptmimizedParams; j++)
                 Foods[i][j] = solution[j];
-            f[i] = ObjValSol;
-            fitness[i] = FitnessSol;
+            f[i] = functionNewVal;
+            fitness[i] = fitnessVal;
         }
         else {
             /* Daca solutia nu poate fi imbunatatita, creste counter-ul de imbunatatire*/
-            trial[i] = trial[i] + 1;
+            improvementFactor[i] = improvementFactor[i] + 1;
         }
     }
 }
@@ -156,15 +155,15 @@ void SendEmployedBees()
 void CalculateProbabilities()
 {
     int i;
-    double maxfit;
-    maxfit = fitness[0];
-    for (i = 1; i < FoodNumber; i++) {
-        if (fitness[i] > maxfit)
-            maxfit = fitness[i];
+    double maxiumumFitness;
+    maxiumumFitness = fitness[0];
+    for (i = 1; i < numberOfFoodResources; i++) {
+        if (fitness[i] > maxiumumFitness)
+            maxiumumFitness = fitness[i];
     }
 
-    for (i = 0; i < FoodNumber; i++) {
-        prob[i] = (0.9 * (fitness[i] / maxfit)) + 0.1;
+    for (i = 0; i < numberOfFoodResources; i++) {
+        probForFoodResToBeChoosen[i] = (0.9 * (fitness[i] / maxiumumFitness)) + 0.1;
     }
 }
 
@@ -174,57 +173,57 @@ void SendOnlookerBees()
     i = 0;
     t = 0;
     /* Etapa albinelor cautatoare*/
-    while (t < FoodNumber) {
+    while (t < numberOfFoodResources) {
 
         r = ((double)rand() / ((double)(RAND_MAX) + (double)(1)));
-        if (r < prob[i]) /* Alege o sursa de mancare in functie de probabilitatea ei de a fi aleasa*/
+        if (r < probForFoodResToBeChoosen[i]) /* Alege o sursa de mancare in functie de probabilitatea acesteia de a fi aleasa*/
         {
             t++;
 
             /* Parametrul care trebuie schimbat este ales aleator*/
             r = ((double)rand() / ((double)(RAND_MAX) + (double)(1)));
-            param2change = (int)(r * D);
+            paramForChange = (int)(r * noOfOptmimizedParams);
 
             /* O solutie aleasa random creaza o solutie derivata a solutiei i*/
             r = ((double)rand() / ((double)(RAND_MAX) + (double)(1)));
-            neighbour = (int)(r * FoodNumber);
+            proximity = (int)(r * numberOfFoodResources);
 
             /* Solutia aleasa random trebuie sa fie diferita de i*/
-            while (neighbour == i) {
+            while (proximity == i) {
                 r = ((double)rand() / ((double)(RAND_MAX) + (double)(1)));
-                neighbour = (int)(r * FoodNumber);
+                proximity = (int)(r * numberOfFoodResources);
             }
-            for (j = 0; j < D; j++)
+            for (j = 0; j < noOfOptmimizedParams; j++)
                 solution[j] = Foods[i][j];
 
             /* v_{ij}=x_{ij}+\phi_{ij}*(x_{kj}-x_{ij})*/
             r = ((double)rand() / ((double)(RAND_MAX) + (double)(1)));
-            solution[param2change] = Foods[i][param2change] + (Foods[i][param2change] - Foods[neighbour][param2change]) * (r - 0.5) * 2;
+            solution[paramForChange] = Foods[i][paramForChange] + (Foods[i][paramForChange] - Foods[proximity][paramForChange]) * (r - 0.5) * 2;
 
             /* Daca valoarea este inafara limitelor se seteaza ca valoare limita*/
-            if (solution[param2change] < lb)
-                solution[param2change] = lb;
-            if (solution[param2change] > ub)
-                solution[param2change] = ub;
-            ObjValSol = function(solution);
-            FitnessSol = CalculateFitness(ObjValSol);
+            if (solution[paramForChange] < lowLimit)
+                solution[paramForChange] = lowLimit;
+            if (solution[paramForChange] > upperLimit)
+                solution[paramForChange] = upperLimit;
+            functionNewVal = function(solution);
+            fitnessVal = CalculateFitness(functionNewVal);
 
             /*a greedy selection is applied between the current solution i and its mutant*/
-            if (FitnessSol > fitness[i]) {
+            if (fitnessVal > fitness[i]) {
                 /* Daca solutia derivata este mai buna decat cea curenta, noua solutie devine cea derivata si reseteaza counter-ul de imbunatatire*/
-                trial[i] = 0;
-                for (j = 0; j < D; j++)
+                improvementFactor[i] = 0;
+                for (int j = 0; j < noOfOptmimizedParams; j++)
                     Foods[i][j] = solution[j];
-                f[i] = ObjValSol;
-                fitness[i] = FitnessSol;
+                f[i] = functionNewVal;
+                fitness[i] = fitnessVal;
             }
             else {
                 /* Daca solutia nu poate fi imbunatatita, creste counter-ul de imbunatatire*/
-                trial[i] = trial[i] + 1;
+                improvementFactor[i] = improvementFactor[i] + 1;
             }
         }
         i++;
-        if (i == FoodNumber)
+        if (i == numberOfFoodResources)
             i = 0;
     }
 }
@@ -232,62 +231,59 @@ void SendOnlookerBees()
 /* Detecteaza sursele pentru care counter-ul de imbunatatire depaseste limita si o abandoneaza*/
 void SendScoutBees()
 {
-    int maxtrialindex, i;
-    maxtrialindex = 0;
-    for (i = 1 ; i < FoodNumber; i++) {
-        if (trial[i] > trial[maxtrialindex])
-            maxtrialindex = i;
+    int maxImprovementFactorIndex, i;
+    maxImprovementFactorIndex = 0;
+    for (i = 1 ; i < numberOfFoodResources; i++) {
+        if (improvementFactor[i] > improvementFactor[maxImprovementFactorIndex])
+            maxImprovementFactorIndex = i;
     }
-    if (trial[maxtrialindex] >= limit) {
-        init(maxtrialindex);
+    if (improvementFactor[maxImprovementFactorIndex] >= limit) {
+        init(maxImprovementFactorIndex);
     }
 }
 
 int main()
 {
-    int iter, run, j;
-    double mean;
-    mean = 0;
+    double globalMinMean;
+    globalMinMean = 0;
     srand(time(NULL));
 
-    for (run = 0; run < runtime; run++) {
+    for (int run = 0; run < numberOfRuns; run++) {
         initial();
         MemorizeBestSource();
-        for (iter = 0; iter < maxCycle; iter++) {
+        for (int iterator = 0; iterator < maxNumberOfCycles; iterator++) {
             SendEmployedBees();
             CalculateProbabilities();
             SendOnlookerBees();
             MemorizeBestSource();
             SendScoutBees();
         }
-        for (j = 0; j < D; j++) {
-            printf("GlobalParam[%d]: %f\n", j + 1, GlobalParams[j]);
+        for (int j = 0; j < noOfOptmimizedParams; j++) {
+           printf("GlobalParam[%d]: %f\n", j + 1, GlobalParams[j]);
         }
         printf("%d. run: %e \n", run + 1, GlobalMin);
         GlobalMins[run] = GlobalMin;
-        mean = mean + GlobalMin;
+        globalMinMean = globalMinMean + GlobalMin;
     }
 
-    mean = mean / runtime;
-    printf("Means of %d runs: %e\n", runtime, mean);
+    globalMinMean = globalMinMean / numberOfRuns;
+    printf("Means of %d runs: %e\n", numberOfRuns, globalMinMean);
 }
 
-double Rosenbrock(double sol[D]) {
-    int j;
+double Rosenbrock(double sol[noOfOptmimizedParams]) {
     double top = 0;
-    for (j = 0; j < D - 1; j++) {
+    for (int j = 0; j < noOfOptmimizedParams - 1; j++) {
         top = top + 100 * pow((sol[j + 1] - pow((sol[j]), (double)2)), (double)2) + pow((sol[j] - 1), (double)2);
     }
     return top;
 }
 
-double Griewank(double sol[D]) {
-    int j;
-    double top1, top2, top;
+double Griewank(double sol[noOfOptmimizedParams]) {
+    double top, top1, top2;
     top = 0;
     top1 = 0;
     top2 = 1;
-    for (j = 0; j < D; j++) {
+    for (int j = 0; j < noOfOptmimizedParams; j++) {
         top1 = top1 + pow((sol[j]), (double)2);
         top2 = top2 * cos((((sol[j]) / sqrt((double)(j + 1))) * M_PI) / 180);
     }
@@ -295,31 +291,19 @@ double Griewank(double sol[D]) {
     return top;
 }
 
-double Rastrigin(double sol[D]) {
-    int j;
+double Rastrigin(double sol[noOfOptmimizedParams]) {
     double top = 0;
 
-    for (j = 0; j < D; j++) {
+    for (int j = 0; j < noOfOptmimizedParams; j++) {
         top = top + (pow(sol[j], (double)2) - 10 * cos(2 * M_PI * sol[j]) + 10);
     }
     return top;
 }
 
-double Ackley(double sol[D]) {
-    int j;
+double Ackley(double sol[noOfOptmimizedParams]) {
     double top = 0;
-    for (j = 0 ; j < D ; j++) {
-        top = top + -20.0*exp(-0.2*sqrt(pow(sol[j], 2) / D )) - exp(cos(2 * pi * sol[D]) / D ) + 20 + e;
+    for (int j = 0 ; j < noOfOptmimizedParams ; j++) {
+        top = top + -20.0*exp(-0.2*sqrt(pow(sol[j], 2) / noOfOptmimizedParams )) - exp(cos(2 * pi * sol[noOfOptmimizedParams]) / noOfOptmimizedParams ) + 20 + e;
     }
     return top;
-}
-
-/* Nu merge*/
-double Schwefel(double sol[D]) {
-    int j;
-    double top = 0;
-    for (j = 0 ; j < D ; j++) {
-        top = top - sol[j] * sin(sqrt(fabs(sol[j])));
-    }
-    return top + alpha * D;
 }
